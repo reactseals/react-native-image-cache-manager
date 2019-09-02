51e8bd879ddebbfe2195e5b0f6de8915e51a5ac8
aurimas535 <aurimasmickys@gmail.com>
2019-09-02 23:16:13 +0300

    updates

--- index.js
+++ index.js
@@ -5,84 +5,79 @@ import RNFS, { DocumentDirectoryPath } from 'react-native-fs';
 const SHA1 = require("crypto-js/sha1");
 const URL = require('url-parse');
 
-
-module.exports = (source = '', options = {}) => {
-    return {
-        deleteCache(filePath) => {
+function deleteCache(filePath) {
+    RNFS
+    .exists(filePath)
+    .then((res) => {
+        if (res) {
             RNFS
-            .exists(filePath)
-            .then((res) => {
-                if (res) {
-                    RNFS
-                    .unlink(filePath)
-                    .catch((err) => {});
-                }
-            });
-        },
+            .unlink(filePath)
+            .catch((err) => {});
+        }
+    });
+}
 
-        processSource(source) {
-            if (source !== null && source != '') {
-                const url = new URL(source, null, true);
+function processSource(source) {
+    if (source !== null && source != '') {
+        const url = new URL(source, null, true);
 
-                // handle query params for cache key
-                let cacheable = url.pathname;
-                cacheable = cacheable.concat(url.query);
+        let cacheable = url.pathname;
+        cacheable = cacheable.concat(url.query);
 
-                const type = url.pathname.replace(/.*\.(.*)/, '$1');
-                const cacheKey = SHA1(cacheable) + (type.length < url.pathname.length ? '.' + type : '');
+        const type = url.pathname.replace(/.*\.(.*)/, '$1');
+        const cacheKey = SHA1(cacheable) + (type.length < url.pathname.length ? '.' + type : '');
 
-                this.checkImageCache(source, url.host, cacheKey);
+        return checkImageCache(source, url.host, cacheKey);
+    }
+
+    return Promise.reject();
+}
+
+function checkImageCache(imageUri, cachePath, cacheKey) {
+    const dirPath = DocumentDirectoryPath+'/'+cachePath;
+    const filePath = dirPath+'/'+cacheKey;
+
+    return RNFS
+        .stat(filePath)
+        .then((res) => {
+            if (res.isFile() && res.size > 0) {
+              return filePath;
+            } else {
+                throw Error("CacheableImage: Invalid file in checkImageCache()");
             }
-        },
-
-        checkImageCache(imageUri, cachePath, cacheKey) {
-            const dirPath = DocumentDirectoryPath+'/'+cachePath;
-            const filePath = dirPath+'/'+cacheKey;
-
-            RNFS
-            .stat(filePath)
-            .then((res) => {
-                if (res.isFile() && res.size > 0) {
-                  return filePath;
-                } else {
-                    throw Error("CacheableImage: Invalid file in checkImageCache()");
-                }
-            })
-            .catch((err) => {
-                RNFS
+        })
+        .catch((err) => {
+            return RNFS
                 .mkdir(dirPath, { NSURLIsExcludedFromBackupKey: true })
                 .then(() => {
                     let downloadOptions = {
                         fromUrl: imageUri,
                         toFile: filePath,
-                        background: this.props.downloadInBackground,
-                        begin: this.imageDownloadBegin,
-                        progress: this.imageDownloadProgress
+                        background: true
                     };
 
                     let download = RNFS.downloadFile(downloadOptions);
                     download.promise
                     .then((res) => {
-                        this.downloading = false;
-                        this.jobId = null;
-
                         switch (res.statusCode) {
                             case 404:
                             case 403:
-                                return null;
+                                return Promise.reject();
                                 break;
                             default:
                                 return filePath;
                         }
                     })
                     .catch((err) => {
-                        this.deleteFilePath(filePath);
+                        deleteFilePath(filePath);
+                        return Promise.reject();
                     });
                 })
                 .catch((err) => {
-                    this.deleteFilePath(filePath);
+                    deleteFilePath(filePath);
+                    return Promise.reject();
                 });
-            });
-        }
-    };
+        });
 }
+
+export default { deleteCache, processSource };
