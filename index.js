cc72729475c77ca6d934e4fbd67f2c9c8596ef23
aurimas535 <aurimasmickys@gmail.com>
2019-09-02 22:53:32 +0300

    update

--- index.js
+++ index.js
@@ -6,89 +6,83 @@ const SHA1 = require("crypto-js/sha1");
 const URL = require('url-parse');
 
 
-module.exports = (path = '', options = {}) => {
+module.exports = (source = '', options = {}) => {
     return {
-      deleteCache(filePath) => {
-          RNFS
-          .exists(filePath)
-          .then((res) => {
-              if (res) {
-                  RNFS
-                  .unlink(filePath)
-                  .catch((err) => {});
-              }
-          });
-      },
+        deleteCache(filePath) => {
+            RNFS
+            .exists(filePath)
+            .then((res) => {
+                if (res) {
+                    RNFS
+                    .unlink(filePath)
+                    .catch((err) => {});
+                }
+            });
+        },
 
-      checkImageCache(imageUri, cachePath, cacheKey) {
-          const dirPath = DocumentDirectoryPath+'/'+cachePath;
-          const filePath = dirPath+'/'+cacheKey;
+        processSource(source) {
+            if (source !== null && source != '') {
+                const url = new URL(source, null, true);
 
-          RNFS
-          .stat(filePath)
-          .then((res) => {
-              if (res.isFile() && res.size > 0) {
-                return filePath;
-              } else {
-                  throw Error("CacheableImage: Invalid file in checkImageCache()");
-              }
-          })
-          .catch((err) => {
-              RNFS
-              .mkdir(dirPath, { NSURLIsExcludedFromBackupKey: true })
-              .then(() => {
+                // handle query params for cache key
+                let cacheable = url.pathname;
+                cacheable = cacheable.concat(url.query);
 
-                  // before we change the cachedImagePath.. if the previous cachedImagePath was set.. remove it
-                  if (this.state.cacheable && this.state.cachedImagePath) {
-                      let delImagePath = this.state.cachedImagePath;
-                      this._deleteFilePath(delImagePath);
-                  }
+                const type = url.pathname.replace(/.*\.(.*)/, '$1');
+                const cacheKey = SHA1(cacheable) + (type.length < url.pathname.length ? '.' + type : '');
 
-                  let downloadOptions = {
-                      fromUrl: imageUri,
-                      toFile: filePath,
-                      background: this.props.downloadInBackground,
-                      begin: this.imageDownloadBegin,
-                      progress: this.imageDownloadProgress
-                  };
+                this.checkImageCache(source, url.host, cacheKey);
+            }
+        },
 
-                  let download = RNFS.downloadFile(downloadOptions);
+        checkImageCache(imageUri, cachePath, cacheKey) {
+            const dirPath = DocumentDirectoryPath+'/'+cachePath;
+            const filePath = dirPath+'/'+cacheKey;
 
-                  this.downloading = true;
-                  this.jobId = download.jobId;
+            RNFS
+            .stat(filePath)
+            .then((res) => {
+                if (res.isFile() && res.size > 0) {
+                  return filePath;
+                } else {
+                    throw Error("CacheableImage: Invalid file in checkImageCache()");
+                }
+            })
+            .catch((err) => {
+                RNFS
+                .mkdir(dirPath, { NSURLIsExcludedFromBackupKey: true })
+                .then(() => {
+                    let downloadOptions = {
+                        fromUrl: imageUri,
+                        toFile: filePath,
+                        background: this.props.downloadInBackground,
+                        begin: this.imageDownloadBegin,
+                        progress: this.imageDownloadProgress
+                    };
 
-                  download.promise
-                  .then((res) => {
-                      this.downloading = false;
-                      this.jobId = null;
+                    let download = RNFS.downloadFile(downloadOptions);
+                    download.promise
+                    .then((res) => {
+                        this.downloading = false;
+                        this.jobId = null;
 
-                      switch (res.statusCode) {
-                          case 404:
-                          case 403:
-                              this.setState({cacheable: false, cachedImagePath: null});
-                              break;
-                          default:
-                              this.setState({cacheable: true, cachedImagePath: filePath});
-                      }
-                  })
-                  .catch((err) => {
-                      // error occurred while downloading or download stopped.. remove file if created
-                      this._deleteFilePath(filePath);
-
-                      // If there was no in-progress job, it may have been cancelled already (and this component may be unmounted)
-                      if (this.downloading) {
-                          this.downloading = false;
-                          this.jobId = null;
-                          this.setState({cacheable: false, cachedImagePath: null});
-                      }
-                  });
-              })
-              .catch((err) => {
-                  this._deleteFilePath(filePath);
-                  this.setState({cacheable: false, cachedImagePath: null});
-              });
-          });
-      }
-
+                        switch (res.statusCode) {
+                            case 404:
+                            case 403:
+                                return null;
+                                break;
+                            default:
+                                return filePath;
+                        }
+                    })
+                    .catch((err) => {
+                        this.deleteFilePath(filePath);
+                    });
+                })
+                .catch((err) => {
+                    this.deleteFilePath(filePath);
+                });
+            });
+        }
     };
 }
