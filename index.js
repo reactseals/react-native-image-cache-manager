bc07f182d976642133b179d83bd1a6750f280438
aurimas535 <aurimasmickys@gmail.com>
2019-09-05 01:22:32 +0300

    fixes and updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import RNFS, { DocumentDirectoryPath } from 'react-native-fs';
+import RNFS, { DocumentDirectoryPath, CachesDirectoryPath, MainBundlePath } from 'react-native-fs';
 
 const SHA1 = require("crypto-js/sha1");
 const URL = require('url-parse');
@@ -21,21 +21,38 @@ function processSource(source) {
     if (source !== null && source != '') {
         const url = new URL(source, null, true);
 
-        let cacheable = url.pathname;
-        cacheable = cacheable.concat(url.query);
+        let cacheable = source;
 
         const type = url.pathname.replace(/.*\.(.*)/, '$1');
-        const cacheKey = SHA1(cacheable) + (type.length < url.pathname.length ? '.' + type : '');
+        let ext;
+
+        if (type === '/') {
+            if (source.match('.png')) {
+                ext = '.png';
+            } else if (source.match('.jpg')) {
+                ext = '.jpg';
+            } else if (source.match('.jpeg')) {
+                ext = '.jpeg';
+            }
+        }
+
+        let cacheKey;
+        if (ext) {
+            cacheKey = `${SHA1(cacheable)}${ext}`;
+        } else {
+            cacheKey = SHA1(cacheable) + (type.length < url.pathname.length ? '.' + type : '');
+        }
+        cacheable = cacheable.concat(url.query);
 
         return checkImageCache(source, url.host, cacheKey);
     }
 
-    return Promise.reject();
+    return Promise.reject('No source');
 }
 
 function checkImageCache(imageUri, cachePath, cacheKey) {
-    const dirPath = DocumentDirectoryPath+'/'+cachePath;
-    const filePath = dirPath+'/'+cacheKey;
+    const dirPath = `${CachesDirectoryPath}/${cachePath}`;
+    const filePath = `${dirPath}/${cacheKey}`;
 
     return RNFS
         .stat(filePath)
@@ -43,7 +60,7 @@ function checkImageCache(imageUri, cachePath, cacheKey) {
             if (res.isFile() && res.size > 0) {
               return filePath;
             } else {
-                throw Error("CacheableImage: Invalid file in checkImageCache()");
+                throw Error("Invalid file");
             }
         })
         .catch((err) => {
@@ -57,25 +74,25 @@ function checkImageCache(imageUri, cachePath, cacheKey) {
                     };
 
                     let download = RNFS.downloadFile(downloadOptions);
-                    download.promise
-                    .then((res) => {
-                        switch (res.statusCode) {
-                            case 404:
-                            case 403:
-                                return Promise.reject();
-                                break;
-                            default:
-                                return filePath;
-                        }
-                    })
-                    .catch((err) => {
-                        deleteFilePath(filePath);
-                        return Promise.reject();
-                    });
+                    return download.promise
+                        .then((res) => {
+                            switch (res.statusCode) {
+                                case 404:
+                                case 403:
+                                    return Promise.reject();
+                                    break;
+                                default:
+                                    return filePath;
+                            }
+                        })
+                        .catch((err) => {
+                            deleteCache(filePath);
+                            return Promise.reject(`Err downloading image ${err}`);
+                        });
                 })
                 .catch((err) => {
-                    deleteFilePath(filePath);
-                    return Promise.reject();
+                    deleteCache(filePath);
+                    return Promise.reject(`Err mkdir ${err}`);
                 });
         });
 }
